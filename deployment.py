3fc80b462927d9aaa793e81c7bff58e1bf325d75
Ajay Sharma <ajsharm@microsoft.com>
2019-01-23 15:53:38 +0530

    File changes

--- deployment.py
+++ deployment.py
@@ -7,6 +7,13 @@ source = "dist"
 destination = "site/wwwroot/"
 config = json.load(open("appcredentials.json", "r"))
 
+def createDirRecursively(dirpath):
+	try:
+		session.mkd(destination+dirpath)
+	except ftplib.error_perm:
+		createDirRecursively('/'.join(dirpath.split("/")[:-1]))
+		session.mkd(destination+dirpath)
+
 session = ftplib.FTP(config["endpoint"], config["username"], config["password"])
 if firstDeployment:
 	file = open("web.config", "rb")
@@ -15,7 +22,11 @@ allFiles = [os.path.join(dp, f).replace("\\", "/") for dp, dn, filenames in os.w
 for f in allFiles:
 	file = open(f, "rb")
 	childname = str.replace(f, source+"/", "")
-	session.storbinary("STOR " + destination + childname, file)
+	try:
+		session.storbinary("STOR " + destination + childname, file)
+	except ftplib.error_perm:
+		createDirRecursively('/'.join(childname.split("/")[:-1]))
+		session.storbinary("STOR " + destination + childname, file)
 	print("Uploaded at ...  ", destination+childname)
 print("Done uploading ", len(allFiles), "files")
 session.quit()
